ff50ede0a6f9f7512e9e881cd73ea6f73a1101f4
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-10 16:02:43 -0700

    mvp complete

--- src/controls/Controls.test.js
+++ src/controls/Controls.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import renderer from "react-test-renderer"; 
 import {render, fireEvent} from '@testing-library/react';
 import Controls from './Controls';
+// import "jest-dom/extend-expect";
+import "@testing-library/jest-dom/extend-expect";
 
 
 describe("<Controls />", () => {
@@ -27,7 +29,38 @@ describe("<Controls />", () => {
         component.getByText(/unlock gate/i);
         component.getByText(/open gate/i);
 
+    });
+
+    it('Shows lock gate and close gate if open and unlocked', ()=>{
+
+      const component = render(<Controls locked={false}
+        closed={false} />);
+        component.getByText(/lock gate/i);
+        component.getByText(/close gate/i);
+
     })
+
+    it('closed button disabled if gate is locked', ()=>{
+
+      const component = render(<Controls locked={true}
+        closed={true} />);
+        let openGate = component.queryByText(/open gate/i);
+        
+        expect(openGate).toHaveAttribute('disabled');
+        
+    });
+
+    it('lock button disabled if gate is open', ()=>{
+
+      const component = render(<Controls locked={false}
+        closed={false} />);
+        
+        let lockButton = component.queryByText(/lock gate/i);
+
+        expect(lockButton).toHaveAttribute('disabled');
+        
+
+    });
 
     // it('wont open if locked', ()=>{
 
